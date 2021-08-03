d7d26e0013723f8cad01f9864bcbdfb6c41960ee
joielee <2j00923@gmail.com>
2021-08-03 19:00:15 +0900

    update project as template for NextJS

---
+++ .next/static/webpack/webpack.88cfbbab63d06bab7d40.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "f0c9bf58133fe104f031"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay44OGNmYmJhYjYzZDA2YmFiN2Q0MC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImYwYzliZjU4MTMzZmUxMDRmMDMxXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-const Index = () => (
-    <div>
-        <p>index page</p>
-    </div>
+import React from "react";
+import Link from "next/link";
+
+const App = () => (
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/components/first">
+          <a>First</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/components/second">
+          <a>Second</a>
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
 
-export default Index;
+export default App;
